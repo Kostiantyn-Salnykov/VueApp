b29bf749ff4fcc533b56811cce8cf1065c6743f8
Kostiantyn Salnykov <kostiantyn.salnykov@gmail.com>
2023-06-15 01:48:18 +0300

    Add ampligy logo

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <a href="https://docs.amplify.aws/" target="_blank">
+      <img src="./assets/amplify.svg" class="logo vue" alt="Amplify logo" />
+    </a>
   </div>
   <HelloWorld msg="Vite + Vue + AWS Amplify + CloudFront" />
 </template>
